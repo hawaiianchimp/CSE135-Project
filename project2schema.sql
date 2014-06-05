c48a242e6d807f45493f50660ce65a3409b9ac4c
vpcombo <vapokala@eng.ucsd.edu>
2014-06-04 22:30:37 -0700

    Updating sql

--- project2schema.sql
+++ project2schema.sql
@@ -38,43 +38,49 @@ CREATE TABLE carts (
 
 CREATE TABLE states (
 	name		TEXT PRIMARY KEY,
-)
+);
+
 
 CREATE TABLE users_total (
 	uid		INTEGER REFERENCES users (id),	
 	total	INTEGER NOT NULL
-)
+);
+INSERT INTO users_total SELECT uid, SUM(quantity*price) FROM sales GROUP BY uid;
+
 
 CREATE TABLE products_total (
 	pid		INTEGER REFERENCES products (id),	
 	total	INTEGER NOT NULL
-)
+);
+INSERT INTO products_total SELECT pid, SUM(quantity*price) FROM sales GROUP BY pid;
 
 CREATE TABLE states_total (
-	state	TEXT REFERENCES users (state),	
+	state	TEXT NOT NULL,
 	total	INTEGER NOT NULL
-)
+);
+INSERT INTO products_total SELECT pid, SUM(quantity*price) FROM sales GROUP BY pid;
 
 CREATE TABLE users_product_total (
 	uid		INTEGER REFERENCES users (id),	
-	pid		INTEGER product products (id),
+	pid		INTEGER REFERENCES products (id),
 	total	INTEGER NOT NULL
-)
+);
+INSERT INTO users_product_total SELECT uid, pid, SUM(quantity*price) FROM sales GROUP BY pid, uid;
 
 CREATE TABLE users_category_total (
 	uid		INTEGER REFERENCES users (id),	
 	cid		INTEGER REFERENCES categories (id),
 	total	INTEGER NOT NULL
-)
+);
 
 CREATE TABLE states_products_total (
-	state 	TEXT REFERENCES users (state),
+	state 	TEXT NOT NULL,
 	pid		INTEGER REFERENCES products (id),
 	total	INTEGER NOT NULL
-)
+);
 
 CREATE TABLE states_categories_total (
-	state 	TEXT REFERENCES users (state),
+	state 	TEXT NOT NULL,
 	cid		INTEGER REFERENCES categories (id),
 	total	INTEGER NOT NULL
-)
+);
