6980b05efe7dc5c63b337f54129ebd08a536eff8
Sean Burke <hawaiianchimp@gmail.com>
2014-06-04 19:31:02 -0700

    adding tables

--- project2schema.sql
+++ project2schema.sql
@@ -39,3 +39,42 @@ CREATE TABLE carts (
 CREATE TABLE states (
 	name		TEXT PRIMARY KEY,
 )
+
+CREATE TABLE users_total (
+	uid		INTEGER REFERENCES users (id),	
+	total	INTEGER NOT NULL
+)
+
+CREATE TABLE products_total (
+	pid		INTEGER REFERENCES products (id),	
+	total	INTEGER NOT NULL
+)
+
+CREATE TABLE states_total (
+	state	TEXT REFERENCES users (state),	
+	total	INTEGER NOT NULL
+)
+
+CREATE TABLE users_product_total (
+	uid		INTEGER REFERENCES users (id),	
+	pid		INTEGER product products (id),
+	total	INTEGER NOT NULL
+)
+
+CREATE TABLE users_category_total (
+	uid		INTEGER REFERENCES users (id),	
+	cid		INTEGER REFERENCES categories (id),
+	total	INTEGER NOT NULL
+)
+
+CREATE TABLE states_products_total (
+	state 	TEXT REFERENCES users (state),
+	pid		INTEGER REFERENCES products (id),
+	total	INTEGER NOT NULL
+)
+
+CREATE TABLE states_categories_total (
+	state 	TEXT REFERENCES users (state),
+	cid		INTEGER REFERENCES categories (id),
+	total	INTEGER NOT NULL
+)
