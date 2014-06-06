00765aa241fb980a1fe40bd10a77268ef41f0a00
Sean Burke <hawaiianchimp@gmail.com>
2014-06-05 19:07:20 -0700

    updating tables

--- project2schema.sql
+++ project2schema.sql
@@ -40,30 +40,46 @@ CREATE TABLE states (
 	name		TEXT PRIMARY KEY,
 );
 
-
+DROP TABLE users_total;
 CREATE TABLE users_total (
 	name    TEXT NOT NULL,
 	uid		INTEGER REFERENCES users (id),	
 	total	INTEGER NOT NULL
 );
-INSERT INTO users_total SELECT uid, SUM(quantity*price) FROM sales GROUP BY uid;
 
+INSERT INTO users_total
+SELECT u.name, u.id, SUM(s.quantity*s.price)
+FROM sales s, users u 
+WHERE s.uid = u.id
+GROUP BY u.name, u.id;
 
+DROP TABLE products_total;
 CREATE TABLE products_total (
 	name    TEXT NOT NULL,
 	pid		INTEGER REFERENCES products (id),	
 	total	INTEGER NOT NULL
 );
-INSERT INTO products_total SELECT pid, SUM(quantity*price) FROM sales GROUP BY pid;
 
+INSERT INTO products_total 
+SELECT p.name, p.id, SUM(s.quantity*s.price) 
+FROM sales s,products p
+WHERE s.pid = p.id
+GROUP BY p.name, p.id;
+
+DROP TABLE states_total;
 CREATE TABLE states_total (
 	name	TEXT NOT NULL,
 	total	INTEGER NOT NULL
 );
-INSERT INTO products_total SELECT pid, SUM(quantity*price) FROM sales GROUP BY pid;
+INSERT INTO states_total 
+SELECT st.name, SUM(s.quantity*s.price) 
+FROM states s, sales st, users u
+WHERE s.name = u.state
+AND u.id = s.uid 
+GROUP BY st.name;
 
+DROP TABLE users_products_total;
 CREATE TABLE users_products_total (
-	name   TEXT NOT NULL,
 	uid		INTEGER REFERENCES users (id),
 	pid		INTEGER REFERENCES products (id),
 	total	INTEGER NOT NULL
@@ -71,6 +87,7 @@ CREATE TABLE users_products_total (
 
 INSERT INTO users_products_total SELECT uid, pid, SUM(quantity*price) FROM sales GROUP BY pid, uid;
 
+DROP TABLE users_categories_total;
 CREATE TABLE users_categories_total (
 	name    TEXT NOT NULL,
 	uid		INTEGER REFERENCES users (id),
@@ -85,6 +102,7 @@ WHERE  c.id = p.cid
 AND s.pid = p.id
 GROUP BY s.uid, c.id;
 
+DROP TABLE states_products_total;
 CREATE TABLE states_products_total (
 	name 	TEXT NOT NULL,
 	pid		INTEGER REFERENCES products (id),
@@ -99,7 +117,7 @@ AND s.pid = p.id
 AND s.uid = u.id
 GROUP BY st.name, p.id;
 
-
+DROP TABLE states_categores_total;
 CREATE TABLE states_categories_total (
 	name 	TEXT NOT NULL,
 	cid		INTEGER REFERENCES categories (id),
