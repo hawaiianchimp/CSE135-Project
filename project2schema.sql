f24762dfaa2a6d3f2c09e836e9f5d99762a07918
Sean Burke <hawaiianchimp@gmail.com>
2014-06-09 17:11:47 -0700

    Reorganizing SQL

--- project2schema.sql
+++ project2schema.sql
@@ -40,37 +40,62 @@ CREATE TABLE states (
 	state		TEXT PRIMARY KEY
 );
 
+
+
+
+
 DROP TABLE users_total;
+DROP TABLE products_total;
+DROP TABLE states_total;
+DROP TABLE users_products_total;
+DROP TABLE users_categories_total;
+DROP TABLE states_products_total;
+DROP TABLE states_categories_total;
+
+
 CREATE TABLE users_total (
 	name    TEXT NOT NULL,
 	uid		INTEGER REFERENCES users (id),	
 	total	INTEGER NOT NULL
 );
+CREATE TABLE products_total (
+	name    TEXT NOT NULL,
+	pid		INTEGER REFERENCES products (id),	
+	total	INTEGER NOT NULL
+);
+CREATE TABLE states_total (
+	state	TEXT NOT NULL,
+	total	INTEGER NOT NULL
+);
+CREATE TABLE users_products_total (
+	uid		INTEGER REFERENCES users (id),
+	pid		INTEGER REFERENCES products (id),
+	total	INTEGER NOT NULL
+);
+
+CREATE TABLE users_categories_total (
+	name    TEXT NOT NULL,
+	uid		INTEGER REFERENCES users (id),
+	cid		INTEGER REFERENCES categories (id),
+	total	INTEGER NOT NULL
+);
+CREATE TABLE states_products_total (
+	state 	TEXT NOT NULL,
+	pid		INTEGER REFERENCES products (id),
+	total	INTEGER NOT NULL
+);
+CREATE TABLE states_categories_total (
+	state 	TEXT NOT NULL,
+	cid		INTEGER REFERENCES categories (id),
+	total	INTEGER NOT NULL
+);
+
 
 INSERT INTO users_total
 SELECT u.name, u.id, SUM(s.quantity*s.price)
 FROM sales s, users u 
 WHERE s.uid = u.id
 GROUP BY u.name, u.id;
-
-DROP TABLE products_total;
-CREATE TABLE products_total (
-	name    TEXT NOT NULL,
-	pid		INTEGER REFERENCES products (id),	
-	total	INTEGER NOT NULL
-);
-
-INSERT INTO products_total 
-SELECT p.name, p.id, SUM(s.quantity*s.price) 
-FROM sales s,products p
-WHERE s.pid = p.id
-GROUP BY p.name, p.id;
-
-DROP TABLE states_total;
-CREATE TABLE states_total (
-	state	TEXT NOT NULL,
-	total	INTEGER NOT NULL
-);
 
 INSERT INTO states_total 
 SELECT st.name, SUM(s.quantity*s.price) 
@@ -79,22 +104,16 @@ WHERE st.name = u.state
 AND u.id = s.uid 
 GROUP BY st.name;
 
-DROP TABLE users_products_total;
-CREATE TABLE users_products_total (
-	uid		INTEGER REFERENCES users (id),
-	pid		INTEGER REFERENCES products (id),
-	total	INTEGER NOT NULL
-);
+INSERT INTO users_products_total 
+SELECT uid, pid, SUM(quantity*price) 
+FROM sales 
+GROUP BY pid, uid;
 
-INSERT INTO users_products_total SELECT uid, pid, SUM(quantity*price) FROM sales GROUP BY pid, uid;
-
-DROP TABLE users_categories_total;
-CREATE TABLE users_categories_total (
-	name    TEXT NOT NULL,
-	uid		INTEGER REFERENCES users (id),
-	cid		INTEGER REFERENCES categories (id),
-	total	INTEGER NOT NULL
-);
+INSERT INTO products_total 
+SELECT p.name, p.id, SUM(s.quantity*s.price) 
+FROM sales s,products p
+WHERE s.pid = p.id
+GROUP BY p.name, p.id;
 
 INSERT INTO users_categories_total 
 SELECT u.name, s.uid, c.id, SUM(s.quantity*s.price) 
@@ -104,25 +123,12 @@ AND s.pid = p.id
 AND s.uid = u.id
 GROUP BY u.name, s.uid, c.id;
 
-DROP TABLE states_products_total;
-CREATE TABLE states_products_total (
-	state 	TEXT NOT NULL,
-	pid		INTEGER REFERENCES products (id),
-	total	INTEGER NOT NULL
-);
-
 INSERT INTO states_products_total
 SELECT u.state, p.pid, SUM(p.total)
 FROM users_products_total p JOIN users u
 ON p.uid = u.id
 GROUP BY u.state, p.pid
 
-DROP TABLE states_categories_total;
-CREATE TABLE states_categories_total (
-	state 	TEXT NOT NULL,
-	cid		INTEGER REFERENCES categories (id),
-	total	INTEGER NOT NULL
-);
 INSERT INTO states_categories_total
 SELECT u.state, pc.cid, SUM(pc.total)
 FROM users u JOIN users_categories_total pc
